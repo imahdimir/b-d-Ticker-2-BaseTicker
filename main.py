6bf2e6420bf2a356cd84289e751dc5eabbede3fb
imahdimir <imahdimir@gmail.com>
2022-08-20 20:32:30 +0430

    Ticker as index

--- main.py
+++ main.py
@@ -6,21 +6,17 @@
 
 ##
 
-import json
-from pathlib import Path
 
 import pandas as pd
-from mirutil.funcs import save_as_prq_wo_index as sprq
-from mirutil.funcs import read_data_according_to_type as rdata
-from mirutil.funcs import norm_fa_str as norm
 from githubdata import GithubData
 
+
+btic_repo_url = 'https://github.com/imahdimir/d-uniq-BaseTickers'
+map_repo_url = 'https://github.com/imahdimir/d-Ticker-2-BaseTicker-map'
 
 btic = 'BaseTicker'
 tick = 'Ticker'
 
-btic_repo_url = 'https://github.com/imahdimir/d-Unique-BaseTickers-TSETMC'
-map_repo_url = 'https://github.com/imahdimir/d-Ticker-2-BaseTicker-map'
 
 ptrns = {
     0    : lambda x : x ,
@@ -44,14 +40,17 @@ def main() :
   btics = GithubData(btic_repo_url)
   btics.clone_overwrite_last_version()
   ##
-  fpn = btics.data_fps[0]
+  bdfpn = btics.data_fps[0]
+  bdf = pd.read_parquet(bdfpn)
+
+  bdf = bdf.reset_index()
+  bdf = bdf[[btic]]
   ##
-  df = rdata(fpn)
   odf = pd.DataFrame()
-  ##
+
   for _ , vl in ptrns.items() :
-    df[tick] = df[btic].apply(vl)
-    odf = pd.concat([odf , df])
+    bdf[tick] = bdf[btic].apply(vl)
+    odf = pd.concat([odf , bdf])
 
   ##
   odf = odf.sort_values([btic , tick])
@@ -60,24 +59,27 @@ def main() :
   tic2btic = GithubData(map_repo_url)
   tic2btic.clone_overwrite_last_version()
   ##
-  tic2btic.set_data_fps()
-  ##
   ofp = tic2btic.data_fps[0]
-  ##
   pre_odf = pd.read_parquet(ofp)
+
+  pre_odf = pre_odf.reset_index()
+  pre_odf = pre_odf[[tick, btic]]
   ##
   odf = pd.concat([odf , pre_odf])
   ##
   odf = odf.drop_duplicates()
   ##
-  sprq(odf , ofp)
+  odf = odf[[tick, btic]]
   ##
-  tic2btic.commit_and_push_to_github_data_target(
-      'init by repo: build-Tickers-fr-BaseTicers-by-ptrn')
+  odf = odf.set_index(tick)
+  ##
+  odf.to_parquet(ofp)
+  ##
+  commit_msg = 'add by repo: https://github.com/imahdimir/build-Tickers-fr-BaseTickers-by-ptrn'
+  tic2btic.commit_and_push_to_github_data_target(commit_msg)
+  ##
 
-  ##
   btics.rmdir()
-  ##
   tic2btic.rmdir()
 
   ##
@@ -93,7 +95,6 @@ if False :
   pass
 
   ##
-  sprq(odf , 'data.prq')
 
   ##
 
